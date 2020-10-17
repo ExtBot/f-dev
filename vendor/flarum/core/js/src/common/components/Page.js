9463c0cb993e953c1ae9aec0a4d570b84e33f005
ExtBot <flarums-com@gooof.de>
2020-10-17 03:03:01 +0200

    composer update 17.10.2020

---
+++ vendor/flarum/core/js/src/common/components/Page.js
@@ -0,0 +1,40 @@
+import Component from '../Component';
+import PageState from '../states/PageState';
+
+/**
+ * The `Page` component
+ *
+ * @abstract
+ */
+export default class Page extends Component {
+  oninit(vnode) {
+    super.oninit(vnode);
+
+    app.previous = app.current;
+    app.current = new PageState(this.constructor, { routeName: this.attrs.routeName });
+
+    app.drawer.hide();
+    app.modal.close();
+
+    /**
+     * A class name to apply to the body while the route is active.
+     *
+     * @type {String}
+     */
+    this.bodyClass = '';
+  }
+
+  oncreate(vnode) {
+    super.oncreate(vnode);
+
+    if (this.bodyClass) {
+      $('#app').addClass(this.bodyClass);
+    }
+  }
+
+  onremove() {
+    if (this.bodyClass) {
+      $('#app').removeClass(this.bodyClass);
+    }
+  }
+}
